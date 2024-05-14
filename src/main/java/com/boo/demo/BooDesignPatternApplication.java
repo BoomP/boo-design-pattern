a7ee9c5269dff8ecf73fc68a01a540c386efd387
王鹏 <wangpeng@zzsymls.com>
2024-05-14 13:20:44 +0800

    初始提交

---
+++ src/main/java/com/boo/demo/BooDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.boo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooDesignPatternApplication.class, args);
+    }
+
+}
